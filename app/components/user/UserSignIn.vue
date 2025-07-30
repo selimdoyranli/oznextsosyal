8505e9ea2bf9dd1247fba6619913c37f23f866e5
selimdoyranli <selimdoyranli@gmail.com>
2025-07-31 00:40:33 +0300

    feat: ux improvements

--- app/components/user/UserSignIn.vue
+++ app/components/user/UserSignIn.vue
@@ -11,14 +11,14 @@ onMounted(async () => {
 <template>
   <form text-center justify-center items-center max-w-150 py6 flex="~ col gap-3" @submit.prevent="oauth">
     <div flex="~ center" items-end mb2 gap-x-2>
-      <img :src="`/${''}logo.svg`" w-12 h-12 mxa height="48" width="48" :alt="$t('app_logo')" class="rtl-flip">
+      <NavLogo class="w-12 h-12" />
       <div text-3xl>
         {{ $t('action.sign_in') }}
       </div>
     </div>
-    <div>
-      {{ $t('user.server_address_label') }}
-    </div>
+    <p text-1xl>
+      mastoturk.org üzerinde giriş yap ya da kaydol, tekrar bu site üzerinde kullanabilirsin.
+    </p>
     <button flex="~ row" gap-x-2 items-center btn-solid mt2>
       {{ $t('action.sign_in') }}
     </button>
